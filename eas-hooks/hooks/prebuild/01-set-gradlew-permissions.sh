15564349ce54c1ed3cad6b3d493eaa55210caf0f
Jonatas-Correa-Dev <jonatasferreiracorrea@gmail.com>
2025-03-23 11:52:12 -0300

    Alpha 5.0

---
+++ eas-hooks/hooks/prebuild/01-set-gradlew-permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Tornar o arquivo gradlew executável
+echo "Dando permissões de execução ao gradlew..."
+chmod +x "$PROJECT_ROOT/android/gradlew"
+echo "Permissões definidas com sucesso." 
